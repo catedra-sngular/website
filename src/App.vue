0d1b802465cf3d4d3bf770e193c7f1beb01c757a
Jorge Mayoral Álvarez <jmayoral@corunet.com>
2022-01-18 12:12:57 +0100

    Add Footer component

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,20 @@
     <el-container>
         <navigation-bar />
         <router-view />
+        <custom-footer />
     </el-container>
 </template>
 
 <script lang="ts">
 import { ElContainer } from 'element-plus'
 import NavigationBar from './components/NavigationBar.vue'
+import Footer from './components/Footer.vue'
 
 export default {
     components: {
         ElContainer,
         NavigationBar,
+        CustomFooter: Footer,
     },
 }
 </script>
@@ -21,5 +24,7 @@ export default {
 .el-container {
     display: flex;
     flex-direction: column;
+    height: 100vh;
+    width: 100%;
 }
 </style>
