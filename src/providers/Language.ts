7462070cfae1f6ed89e56965f73d6856dd21b56f
rsanfer <rsanchez@corunet.com>
2022-02-10 15:43:57 +0100

    Added license.

--- src/providers/Language.ts
+++ src/providers/Language.ts
@@ -1,3 +1,20 @@
+/*
+Copyright (C) 2022 Catedra Corunet polo Impulso da Ciencia Aberta
+                   a través do Software
+
+This program is free software: you can redistribute it and/or modify it 
+under the terms of the GNU General Public License as published by the Free 
+Software Foundation, either version 3 of the License, or (at your option) 
+any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with 
+this program. If not, see <https://www.gnu.org/licenses/>. 
+*/
+
 import { computed, defineComponent, provide, readonly, Ref, ref } from 'vue'
 import localeGL from '@/locales/GL.json'
 import localeES from '@/locales/ES.json'
